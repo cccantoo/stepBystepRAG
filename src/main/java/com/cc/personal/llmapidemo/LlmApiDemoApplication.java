0101729dbcd3dbe412daae2e786a714150b48da1
ccPopo <2352619468@qq.com>
2026-04-30 21:40:05 +0800

    流式调用和非流式调用及prompt工程

---
+++ src/main/java/com/cc/personal/llmapidemo/LlmApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cc.personal.llmapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmApiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cc/personal/llmapidemo/LlmApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.personal.llmapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
